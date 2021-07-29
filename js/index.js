2b699ece86d59d15b55f30ac80835bb2dc43a9a3
SvitlanaPan <sveto4kahappy@gmail.com>
2021-07-29 00:22:04 -0500

    Added files index.html and js/index.js

--- js/index.js
+++ js/index.js
@@ -17,8 +17,46 @@ copyright.innerHTML = Svitlana Pankiv ${thisYear};
 copyright.className = "copyright";
 footer.appendChild(copyright);
 
+const messageForm = document.getElementsByName('leave_message') [0];
 
 
+messageForm.addEventListener("submit",(event) => {
+    event.preventDefault();
+    const inputElements = document.querySelectorAll('input, textarea');
+    const name = inputElements[0].value;
+    const email = inputElements[1].value;
+    const message = inputElements[2].value;
+    console.log(name, email, message);
+    
+    const messageSection = document.getElementById('messages');
+    const messageList = messageSection.querySelector('ul');
+    const newMessage = document.createElement('li');
+    console.log(newMessage);
+     
+    const linkMessage = document.createElement('a');
+    const spanMessage = document.createElement('span');
+    linkMessage.textContent = name;
+    linkMessage.href = `mailto:${email}`;
+    spanMessage.textContent = ` ${message} `;
+
+    newMessage.appendChild(linkMessage);
+    newMessage.appendChild(spanMessage);
+
+
+    const removeButton = document.createElement('button');
+    removeButton.textContent = 'remove';
+    removeButton.type = 'button';
+    removeButton.addEventListener('click', () => {
+        const entry = removeButton.parentNode.parentNode;
+        entry.removeChild(newMessage);
+    })
+
+    messageList.appendChild(newMessage);
+    newMessage.appendChild(removeButton);
+
+    mesageForm.reset();
+
+});
 
 
 
